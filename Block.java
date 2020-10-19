337d63159b85cc59dfd2f55219ac929b1d3c8902
gallo-json <gallojsantiago@gmail.com>
2020-10-19 09:53:26 -0500

    triggered tarek

--- Block.java
+++ Block.java
@@ -1,5 +1,15 @@
+import java.security.NoSuchAlgorithmException;  
+
 public class Block {
-    public static void main(String[] args) {
-        System.out.println("java works");
-    }
+    public static void main(String args[]) {  
+        try { 
+            String s1 = "cock";  
+            System.out.println("\n" + s1 + " : " + Hash.toHexString(Hash.getSHA(s1)));  
+        } 
+        // For specifying wrong message digest algorithms  
+        catch (NoSuchAlgorithmException e) {  
+            System.out.println("Exception thrown for incorrect algorithm: " + e);  
+        }  
+    }  
+    
 }
