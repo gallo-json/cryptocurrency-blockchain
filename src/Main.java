346e05383a42474f654216b1efb356fe8ede4458
gallo-json <gallojsantiago@gmail.com>
2020-10-21 10:55:48 -0500

    OMETHING WORKS

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,17 @@ public class Main {
             Blockchain pepegaCoin = new Blockchain();
             SigningKeys signingKeys = new SigningKeys();
             KeyPair myKeys = signingKeys.generate();
+            String myWalletAddress = signingKeys.toString(myKeys.getPublic());
+
+            Transaction tx1 = new Transaction(10, myWalletAddress, "reciever public key");
+            tx1.signTransaction(signingKeys, myKeys);
+            pepegaCoin.addTransaction(tx1);
+            pepegaCoin.printAsString();
+
+            System.out.println("Starting the miner...");
+            pepegaCoin.minePendingTransactions(myWalletAddress);
+
+            System.out.println("Balance of the miner is now " + pepegaCoin.getBalance(myWalletAddress));
         } catch (Exception e) {
             System.out.println(e);
         }
